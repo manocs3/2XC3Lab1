e4a32e3fa2572012e630e54970ef47249b011c57
Jialiang Wu <wu91@mcmaster.ca>
2022-01-20 15:04:27 -0500

    adding JWcode.py

---
+++ JWcode.py
@@ -0,0 +1,40 @@
+studentNum = [1,2,3,4,5,6,7]
+groups = [[1,3],[2,4,5,5],[6,7]]
+
+def are_valid_groups(studentNum, groups):
+    count = 0
+    for ID in studentNum:
+        for group in groups:
+            for id in group:
+                if  ID == id:
+                    count = count + 1
+    if count == len(studentNum):
+        print("True") 
+    else:
+        print("Flase")
+    
+
+def goodGroups(groups):
+    flattenedGroups = []
+    validgroups = 0
+    invalidgroups =[]
+    
+    for group in groups:
+        for i in group:
+            flattenedGroups.append(i)
+    
+    for group in groups:
+        if ((len(group) == 2) or (len(group) == 3)):
+            validgroups = validgroups + 1
+        else:
+            invalidgroups.append(group)
+    
+    if (len(flattenedGroups) == len(set(flattenedGroups))) and (validgroups == len(groups)):
+        print("Ture! Each student ids appears exactly once in the group list and each group has 2-3 people")
+    else:
+        print("here is/are group(s) which doesnt/dont have 2-3 people:" ) 
+        print(invalidgroups)
+
+        
+are_valid_groups(studentNum, groups)
+goodGroups(groups)
